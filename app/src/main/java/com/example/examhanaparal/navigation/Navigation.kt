569cf7f15b1c1d9cabb20a8e090a3959463ca55f
Cativo Ljie <cativoljie501@gmail.com>
2026-03-26 21:00:05 +0800

    Initial navigation , fixing gradles and libtoml

--- app/src/main/java/com/example/examhanaparal/navigation/Navigation.kt
+++ app/src/main/java/com/example/examhanaparal/navigation/Navigation.kt
@@ -1,2 +1,18 @@
 package com.example.examhanaparal.navigation
 
+import androidx.compose.runtime.Composable
+
+object Routes{
+    const val LOGIN = "login"
+    const val PROFILE_SETUP = "profile_setup"
+    const val MAIN = "main"
+    const val CREATE_GROUP = "create_group"
+    const val GROUP_DETAIL = "group_detail"
+    const val USER_PROFILE = "user_profile"
+    const val ADMIN = "admin"
+
+    // Helper function to build a path with an ID
+    fun groupDetail(groupId: String) = "group_detail/$groupId"
+
+}
+
